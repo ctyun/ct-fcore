83e68d049ef659403acfe5d723cb9b3be640a1da
WangYG <wangyonggang@ctyun.cn>
2016-06-23 15:18:21 +0800

    增加方法

--- ct_fcore/utils/formater.py
+++ ct_fcore/utils/formater.py
@@ -34,3 +34,14 @@ def jsdate2str(jsdate, fotmatstr="%Y-%m-%d", tz=8):
     ts = datetime.datetime.strptime(jsdate.split(".")[0], "%Y-%m-%dT%H:%M:%S") + datetime.timedelta(hours=tz)
     return ts.strftime(fotmatstr)
 
+def jsdate2ts(jsdate, fotmatstr="%Y-%m-%d", tz=8):
+    '''
+    将js的date对象转换成str
+    :param jsdate: jsdate对象直接json序列化的字符串
+    :param fotmatstr: 返回值的格式
+    :param tz: 时区, 默认为+08:00, 即东八区
+    :return: 时间戳
+    '''
+    ts = datetime.datetime.strptime(jsdate.split(".")[0], "%Y-%m-%dT%H:%M:%S") + datetime.timedelta(hours=tz)
+    return int(time.mktime(ts.timetuple()))
+
