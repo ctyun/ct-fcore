9a738f7bf6322d05a3f933a4eebbd022fcb95f5d
WangYG <wangyonggang@ctyun.cn>
2016-07-27 14:23:12 +0800

    更新依赖

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'ct_fcore',
-    version = '0.1.12',
+    version = '0.1.13',
     keywords = ('django', 'ctcloud', 'portal', 'ct_fcore', 'chinatelecom'),
     description = 'a portal framework for ctcloud using tornado',
     license = 'MIT License',
-    install_requires = ['tornado>=4.3'],
+    install_requires = ['tornado==4.3'],
 
     author = 'astwyg',
     author_email = 'i@ysgh.net',
